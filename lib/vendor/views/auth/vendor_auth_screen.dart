c6125b1f43173aa058f7a07517f50ca5b260f405
JackICT16✅ <139789768+JACKICT16@users.noreply.github.com>
2023-09-06 20:16:47 +0700

    first commit

---
+++ lib/vendor/views/auth/vendor_auth_screen.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/material.dart';
+
+class VendorAuthScreen extends StatefulWidget {
+  const VendorAuthScreen({super.key});
+
+  @override
+  State<VendorAuthScreen> createState() => _VendorAuthScreenState();
+}
+
+class _VendorAuthScreenState extends State<VendorAuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      initialData: FirebaseAuth.instance.currentUser,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return SignInScreen(
+            providers: [
+              EmailAuthProvider(),
+            ],
+          );
+        }
+
+        return ElevatedButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+            },
+            child: Text('Sign Out'));
+      },
+    );
+  }
+}
